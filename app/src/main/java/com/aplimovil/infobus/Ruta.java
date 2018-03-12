04b5e14cf970200f9b1b0d46c4c6039a16ad8bc7
Daniel Eduardo Collazos García <cgdaniel@unicauca.edu.co>
2018-03-12 14:38:02 -0500

    Initial commit

---
+++ app/src/main/java/com/aplimovil/infobus/Ruta.java
@@ -0,0 +1,13 @@
+package com.aplimovil.infobus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ruta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ruta);
+    }
+}
